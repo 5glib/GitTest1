3cc4c11f42524b921965aca380d213a44c3962a5
touch <mymail@gmail.com>
2018-06-03 16:17:43 +0100

    new code

--- ConsoleApp1/ConsoleApp1/feature1.cs
+++ ConsoleApp1/ConsoleApp1/feature1.cs
@@ -8,5 +8,12 @@ namespace ConsoleApp1
 {
     class feature1
     {
+        public int add()
+        {
+            int x1 = 10;
+            int x2 = 20;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
